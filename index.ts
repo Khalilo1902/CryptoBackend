ea1e49056e8c7f046cf06e5129041fe4c295d7fe
khalil <khalil.haouas@gmail.com>
2024-10-06 21:04:31 +0200

    finish

--- index.ts
+++ index.ts
@@ -19,8 +19,8 @@ dbConnect();
 const app = express();
 
 app.use(express.json());
-app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
-//app.use(cors({ credentials: true, origin: "https://orosia.online" }));
+//app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
+app.use(cors({ credentials: true, origin: "https://khalil-dev.me" }));
 app.use(cookieParser());
 
 app.use(userRouter);
