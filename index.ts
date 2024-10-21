9077896d33b609e9a2b73b790dfe231b14ed9f98
khalil <khalil.haouas@gmail.com>
2024-10-21 11:39:17 +0200

    update

--- index.ts
+++ index.ts
@@ -20,6 +20,7 @@ const app = express();
 
 app.use(express.json());
 //app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
+
 app.use(cors({ credentials: true, origin: "https://khalil-dev.me" }));
 app.use(cookieParser());
 
